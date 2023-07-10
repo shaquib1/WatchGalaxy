26776fa3e520af7484716f537947e6f523017f84
Md Shaquib Alam <shaquib475@gmail.com>
2023-07-10 23:34:10 +0530

    Server.js

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 
 const express =require('express');
+const dotenv=require('dotenv');
+
+
+// configure env
+dotenv.config();
 
 //rest object
 const app=express();
 
 //rest api building example
 app.get('/' , (req, res)=>{
-    res.send("hello this is my ecommerce website");
+    res.send("hello this is my ecommerce full stack web application");
 });
 
 
 //port
- const port=8000;
+ const port=process.env.PORT;
 
  //run or listen app
  app.listen(port ,()=>{
