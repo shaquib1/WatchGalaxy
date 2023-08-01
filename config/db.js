ebddf643767a564f4d94d9660c533d13116bac2c
Md Shaquib Alam <shaquib475@gmail.com>
2023-08-01 14:00:09 +0530

    Colors added

--- config/db.js
+++ config/db.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
-import Color from "color";
+import Colors from "colors";
 
 
 const connectDB= async()=>{
     try{ 
   
    const conn = await mongoose.connect(process.env.MONGO_URL);
-    console.log(`Connected to mongodb database ${conn.connection.host}`);
+    console.log(`Connected to mongodb database ${conn.connection.host}`.bgCyan.white);
     } catch(error){
       console.log(`error in mongodb ${error}`);
     }
