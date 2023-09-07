1e6833b4232eae5fd73955b2d03c9f7fa0ec1209
Md Shaquib Alam <shaquib475@gmail.com>
2023-09-08 00:09:03 +0530

    server

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
-
-import express  from "express";  //this is es6 module features import and export        // const express =require('express');
-import Color from "color";         //const colors= require('color');
-import dotenv from "dotenv";           // const dotenv=require('dotenv');
+import express from "express"; //this is es6 module features import and export        // const express =require('express');
+import Color from "color"; //const colors= require('color');
+import dotenv from "dotenv"; // const dotenv=require('dotenv');
 import morgan from "morgan";
 import connectDB from "./config/db.js";
-import authRoutes from "./routes/authRoute.js"
-import categoryRoute from  './routes/categoryRoutes.js'
-import productRoute from './routes/productRoutes.js'
-import cors from 'cors';
-import path from 'path';
+import authRoutes from "./routes/authRoute.js";
+import categoryRoute from "./routes/categoryRoutes.js";
+import productRoute from "./routes/productRoutes.js";
+import cors from "cors";
+import path from "path";
 
 //rest object
-const app=express();    // here .env file is root file so we not use path but if this file inside any folder that use in config(path:'path_name')
+const app = express(); // here .env file is root file so we not use path but if this file inside any folder that use in config(path:'path_name')
 
 // middelwares
-app.use(cors());        // call cors or use cors
+app.use(cors()); // call cors or use cors
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 //app.use(express.static(path.join(__dirname,'./client/build')))  // note when host this application then remove this line of code from comment to code then it is working
 
 // configure env
@@ -25,22 +24,23 @@ dotenv.config();
 // databse config
 connectDB();
 
-
 // routes
-app.use('/api/v1/auth',authRoutes);
-app.use('/api/v1/category' , categoryRoute)
-app.use('/api/v1/product' , productRoute)
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/category", categoryRoute);
+app.use("/api/v1/product", productRoute);
 
-
-//rest api for hosting 
-app.use('*' , function(req , res){
-    res.sendFile(path.join(__dirname,'./client/build/index.html'));
-})
+//rest api for hosting
+app.use("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 //port
- const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8000;
 
- //run or listen app
- app.listen(PORT ,()=>{
-    console.log(`server is running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgBlack.white);
- });
+//run or listen app
+app.listen(PORT, () => {
+  console.log(
+    `server is running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgBlack
+      .white
+  );
+});
